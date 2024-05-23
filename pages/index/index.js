7d307a1f0c3618b030b06525c9ae8260f979f897
miaoyurun <miaoyurun@bytedance.com>
2024-05-23 10:47:53 +0800

    增加页面分组导航

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,28 @@
 Page({
-  data: {},
+  data: {
+    list: [
+      {
+        id: "common",
+        name: "通用",
+        pages: [{ name: "长文本分页", url: "/pages/pagination/pagination" }],
+      },
+    ],
+  },
+
+  toggle: function (e) {
+    const id = e.currentTarget.dataset.id;
+    const list = this.data.list;
+
+    for (let index = 0; index < list.length; index++) {
+      if (list[index].id == id) {
+        list[index].open = !list[index].open;
+      } else {
+        list[index].open = false;
+      }
+    }
+
+    this.setData({
+      list,
+    });
+  },
 });
